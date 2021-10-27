14d8352d6e2f8eb8b6790d856f39f7e33b4c4247
g <g>
2021-10-27 16:00:17 -0400

    presque fini, a rajouter le toaster

--- Lapin/app/src/main/java/com/example/lapin/MainActivity.java
+++ Lapin/app/src/main/java/com/example/lapin/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.lapin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.lapin.databinding.ActivityMainBinding;
 
@@ -12,16 +14,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    List<Button> bouttons = new ArrayList<>();
+    int positionLapin;
+    int pafs =0;
+    int flops = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
-        List<Button> bouttons = new ArrayList<>();
+
         bouttons.add(binding.btn1);
         bouttons.add(binding.btn2);
         bouttons.add(binding.btn3);
@@ -32,11 +39,19 @@ public class MainActivity extends AppCompatActivity {
         bouttons.add(binding.btn8);
         bouttons.add(binding.btn9);
 
-        Random random = new Random();
-        int NombreHasard = random.nextInt(9);
-        bouttons.get(NombreHasard).setText("Lapin");
 
-        
+        bougerLeLapin();
+
+        for (Button boutton:bouttons
+             ) {
+           boutton.setOnClickListener(view -> {
+
+                reagirClick(view);
+
+            });
+
+        }
+
 
 
 
@@ -44,4 +59,34 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void bougerLeLapin() {
+
+        Random random = new Random();
+        positionLapin = random.nextInt(9);
+        bouttons.get(positionLapin).setText("Lapin");
+
+    }
+
+    private void reagirClick(View view) {
+
+        Button buttonLapin = bouttons.get(positionLapin);
+
+        if (view==buttonLapin)
+        {
+            pafs++;
+            binding.pafs.setText(pafs + " pafs");
+            bougerLeLapin();
+        }
+        else
+            {
+                flops++;
+                binding.flops.setText(flops + " flops");
+
+
+            }
+
+
+    }
+
+
 }
